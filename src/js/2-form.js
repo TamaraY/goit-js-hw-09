8ec492c688da4f71e9fb347c035ab4a6f2e571fc
TamaraY <tamasik99@gmail.com>
2024-08-06 09:11:08 +0300

    added tasks from homework 9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,48 @@
-console.log('Form');
+const STORAGE_KEY = 'feedback-form';
+
+const form = document.querySelector('.feedback-form');
+
+populateForm();
+
+form.addEventListener('submit', handleFormSubmit);
+form.addEventListener('input', handleFormInput);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = {
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
+  };
+
+  if (!formData.email || !formData.message) {
+    alert('Please fill all fields.');
+    return;
+  }
+
+  console.log('Form data:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+function handleFormInput(event) {
+  const value = event.target.value;
+  const key = event.target.name;
+
+  let savedFeedbackData = {};
+
+  try {
+    savedFeedbackData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+}
+
+function populateForm() {
+  let savedFeedbackData = {};
+
+  form.elements.email.value = savedFeedbackData.email || '';
+  form.elements.message.value = savedFeedbackData.message || '';
+}
